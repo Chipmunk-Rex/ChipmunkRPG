b52ff9af3ceede0518ee100f4102a66e98d581d3
Chipmunk-Rex <rexgood1005@gmail.com>
2024-09-25 20:41:25 +0900

    feat : BaseItemSO

---
+++ Assets/ItemEditor/Item/BaseItemSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseItemSO : ScriptableObject
+{
+    [SerializeField] public string itemName = "Item";
+    [SerializeField] public Sprite itemSprite;
+    public EnumItemRarity enumItemRarity = EnumItemRarity.None;
+    public abstract void Initialize();
+}
---
+++ Assets/ItemEditor/Item/EnumItemRarity.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnumItemRarity
+{
+    None, Common, Rare, Unique, Legend
+}
