8fb7ba38283fad8b0e44bf173170099bfbb0f617
Chipmunk-Rex <rexgood1005@gmail.com>
2024-09-30 15:44:45 +0900

    feat : PerlineNoise

--- Assets/01.Scripts/MapSystem/Noise/PerlinNoise.cs
+++ Assets/01.Scripts/MapSystem/Noise/PerlinNoise.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class PerlinNoise
 {
-    private int offset;
-    public PerlinNoise()
+    private float offset;
+    private int seed;
+    private float scale;
+    public PerlinNoise(float scale, int seed)
     {
+        this.seed = seed;
+        this.scale = scale;
 
+        Random.InitState(seed);
+        offset = Random.Range(-100, 100);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns>0 ~ 1 float</returns>
+    public float CalculateNoise(Vector2 position)
+    {
+        float xCoord = position.x / scale + offset;
+        float yCoord = position.y / scale + offset;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
     }
 }
--- Assets/01.Scripts/TestPer.cs
+++ Assets/01.Scripts/TestPer.cs
@@ -1,23 +1,19 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TestSprite : MonoBehaviour
+public class TestPer : MonoBehaviour
 {
     [SerializeField] private RawImage rawImage;
     [SerializeField] Color[] posibleColor;
-    VoronoiNoise voronoiNoise;
-    [SerializeField] int cellSize = 3;
+    PerlinNoise perlinNoise;
+    [SerializeField] float scale = 10;
     [SerializeField] int seed;
     private void Awake()
     {
-        voronoiNoise = new(cellSize, seed);
+        perlinNoise = new(scale, seed);
         Genarate();
-    }
-    private void Update() {
-        
     }
 
     private void Genarate()
@@ -30,11 +26,12 @@ public class TestSprite : MonoBehaviour
         for (int x = 0; x < size; x++)
             for (int y = 0; y < size; y++)
             {
-                int value = voronoiNoise.CalculateNoise(new Vector2Int(x, y));
-                int colorIndex = value / (int.MaxValue / posibleColor.Length);
+                float value = perlinNoise.CalculateNoise(new Vector2(x, y) / 2);
+                value = Mathf.Clamp(value, 0, 0.8f);
+                int colorIndex = Mathf.RoundToInt((value / (1 / (float)posibleColor.Length)) * 10)/ 10;
+                Debug.Log((1 / (float)posibleColor.Length));
+                Debug.Log($"{colorIndex} , {value}");
                 Color color = posibleColor[colorIndex];
-                if(voronoiNoise.isCell(new Vector2Int(x, y)))
-                    color = Color.black;
                 texture.SetPixel(x, y, color);
             }
 
