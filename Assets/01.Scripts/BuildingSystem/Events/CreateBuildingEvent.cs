33e1859149989f26775895435bd4f3479f1bbd1f
Chipmunk-Rex <rexgood1005@gmail.com>
2024-10-03 18:03:27 +0900

    fix : Create Building Event
    * TileSo에서 범위를 벗어난 타일이 설치되는 수정

--- Assets/01.Scripts/BuildingSystem/Events/CreateBuildingEvent.cs
+++ Assets/01.Scripts/BuildingSystem/Events/CreateBuildingEvent.cs
@@ -12,23 +12,47 @@ public class CreateBuildingEvent : BuildingEvent
         if (!world.CanBuild(pos, building.buildingSO))
             return EnumEventResult.Failed;
 
-        foreach (KeyValuePair<Vector2Int, TileBase> keyValue in building.buildingSO.tileDatas)
+        for (int x = -building.buildingSO.left; x <= building.buildingSO.right; x++)
         {
-            Vector2Int tilePos = pos + keyValue.Key;
-            Debug.Log($"설치 {tilePos}");
-            TileBase tile = keyValue.Value;
-
-            Ground ground = world.GetGround(tilePos);
-            if (ground == null)
+            for (int y = -building.buildingSO.down; y <= building.buildingSO.top; y++)
             {
-                Debug.LogError($"CreateBuildingEvent : ground value is null!!");
-                return EnumEventResult.Failed;
-            }
-            Debug.Log($"{tilePos} {world.GetBuilding(tilePos) == null}");
-            ground.building = building;
+                Vector2Int tilePos = new Vector2Int(x, y);
+                if (building.buildingSO.tileDatas.ContainsKey(tilePos) == false)
+                    continue;
 
-            world.buildingTilemap.SetTile((Vector3Int)tilePos, tile);
+                Vector2Int tileWorldPos = pos + tilePos;
+                Debug.Log($"설치 {tileWorldPos}");
+                TileBase tile = building.buildingSO.tileDatas[tilePos];
+
+                Ground ground = world.GetGround(tileWorldPos);
+                if (ground == null)
+                {
+                    Debug.LogError($"CreateBuildingEvent : ground value is null!!");
+                    return EnumEventResult.Failed;
+                }
+                Debug.Log($"{tileWorldPos} {world.GetBuilding(tileWorldPos) == null}");
+                ground.building = building;
+
+                world.buildingTilemap.SetTile((Vector3Int)tileWorldPos, tile);
+            }
         }
+        // foreach (KeyValuePair<Vector2Int, TileBase> keyValue in building.buildingSO.tileDatas)
+        // {
+        //     Vector2Int tilePos = pos + keyValue.Key;
+        //     Debug.Log($"설치 {tilePos}");
+        //     TileBase tile = keyValue.Value;
+
+        //     Ground ground = world.GetGround(tilePos);
+        //     if (ground == null)
+        //     {
+        //         Debug.LogError($"CreateBuildingEvent : ground value is null!!");
+        //         return EnumEventResult.Failed;
+        //     }
+        //     Debug.Log($"{tilePos} {world.GetBuilding(tilePos) == null}");
+        //     ground.building = building;
+
+        //     world.buildingTilemap.SetTile((Vector3Int)tilePos, tile);
+        // }
 
         building.pos = pos;
         building.currentWorld = world;
