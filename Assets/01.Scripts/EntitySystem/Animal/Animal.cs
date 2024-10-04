7e9a6aa16e6ee18fd20bcbc4939c048b347b8d10
Chipmunk-Rex <rexgood1005@gmail.com>
2024-10-04 22:48:51 +0900

    feat : Desert Biome, fix : change ChipmunkBT to GGMBT

--- Assets/01.Scripts/EntitySystem/Animal/Animal.cs
+++ Assets/01.Scripts/EntitySystem/Animal/Animal.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[RequireComponent(typeof(BT_BehaviourTreeExecutor))]
 public class Animal : Entity
 {
 }
--- Assets/EntityEditor/Script/BTEntitySO.cs
+++ Assets/EntityEditor/Script/BTEntitySO.cs
@@ -4,5 +4,4 @@ using UnityEngine;
 
 public class BTEntitySO : EntitySO
 {
-    public BT_BehaviourTree behaviourTree;
 }
