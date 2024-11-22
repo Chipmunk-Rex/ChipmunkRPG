966e5669a53f01122edafde32cca12e4b655dd56
Chipmunk-Rex <rexgood1005@gmail.com>
2024-11-22 23:57:43 +0900

    fix: : remove not used script

--- Assets/01.Scripts/SaveLoadSystem/IJsonSerializeAble.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using Newtonsoft.Json;
-
-public interface IJsonSerializeAble
-{
-    void DeSerialize(JsonReader reader, JsonSerializer serializer);
-    string OnSerialize(JsonWriter writer, JsonSerializer serializer);
-}
